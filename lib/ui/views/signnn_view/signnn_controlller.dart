f73bf2000b5f2e966ca5aec5acfb47df24142098
InasAlSaabb <enassaab71@gmail.com>
2024-03-10 21:23:33 +0300

    ..

--- lib/ui/views/signnn_view/signnn_controlller.dart
+++ lib/ui/views/signnn_view/signnn_controlller.dart
@@ -4,6 +4,7 @@ import 'package:flutter_templete/core/enums/message_type.dart';
 import 'package:flutter_templete/core/services/base_controller.dart';
 import 'package:flutter_templete/ui/shared/custom_widgets/custom_toast.dart';
 import 'package:flutter_templete/ui/views/varification_code/varification_cod_view.dart';
+
 import 'package:get/get.dart';
 
 class SignnnUpController extends BaseController {
